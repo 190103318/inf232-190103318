d9d274f27f8df83ad87cf6284927f24f9517cbf0
Arna Bukharbayeva <190103318@stu.sdu.edu.kz>
2021-02-08 18:22:15 +0600

    Code for PHP conditions

---
+++ ConditionsPHP.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP code in GitHub</h1>
+
+<?php
+echo "Coding is fun!<br>"; 
+$hour = date("H");
+
+if ($hour < "10") {
+  echo "Have a good morning!";
+} elseif ($hour < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
+
+</body>
+</html>
