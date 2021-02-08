8eb9ce4ad6f150820d8e571ffaea1ef7863c7a1d
Arna Bukharbayeva <190103318@stu.sdu.edu.kz>
2021-02-08 19:04:43 +0600

    Update for CodePHP

--- CodePHP.php
+++ CodePHP.php
@@ -5,30 +5,31 @@
 <h1>My first PHP code in GitHub</h1>
 
 <?php
-echo "Hello!";
+echo "Hello!<br>";
+echo "This code returns which day is it today.<hr>";
 $day = date("w");
 
 switch ($day) {
   case 0:
-    echo "Today is Sunday!";
+    echo "Today is Sunday! Relax today properly to work all week!";
     break;
   case 1:
-    echo "Today is Monday!";
+    echo "Today is Monday! Work harder!";
     break;
   case 2:
-    echo "Today is Tuesday!";
+    echo "Today is Tuesday!Work harder again!";
     break;
   case 3:
-    echo "Today is Wednesday!";
+    echo "Today is Wednesday! Continue working!";
     break;
   case 4:
-    echo "Today is Thursday!";
+    echo "Today is Thursday! Try to work!";
     break;
   case 5:
-    echo "Today is Friday!";
+    echo "Today is Friday! Just one day left for weekends!";
     break;
   case 6:
-    echo "Today is Saturday!";
+    echo "Today is Saturday! You should relax today!";
     break;
   default:
     echo "You are in a parallel universe where the days of the week are counted differently...";
